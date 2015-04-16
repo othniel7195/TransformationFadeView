02c1bb53d14788d796eca0c79f0814fb3daaeeae
YouXianMing <705786299@qq.com>
2015-04-16 15:41:59 +0800

    add demo

--- TransformationFadeView/ViewController.m
+++ TransformationFadeView/ViewController.m
@@ -7,8 +7,20 @@
 //
 
 #import "ViewController.h"
+#import "TranformFadeView.h"
+
+typedef enum : NSUInteger {
+    TYPE_ONE,
+    TYPE_TWO,
+} EType;
 
 @interface ViewController ()
+
+@property (nonatomic, strong) TranformFadeView *tranformFadeViewOne;
+@property (nonatomic, strong) TranformFadeView *tranformFadeViewTwo;
+
+@property (nonatomic, strong) NSTimer          *timer;
+@property (nonatomic)         EType             type;
 
 @end
 
@@ -16,12 +28,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor blackColor];
+
+    // 图片1
+    self.tranformFadeViewOne                 = [[TranformFadeView alloc] initWithFrame:self.view.bounds];
+    self.tranformFadeViewOne.contentMode     = UIViewContentModeScaleAspectFill;
+    self.tranformFadeViewOne.image           = [UIImage imageNamed:@"1"];
+    self.tranformFadeViewOne.verticalCount   = 5;
+    self.tranformFadeViewOne.horizontalCount = 5;
+    self.tranformFadeViewOne.center          = self.view.center;
+    [self.tranformFadeViewOne buildMaskView];
+    
+    self.tranformFadeViewOne.fadeDuradtion        = 1.f;
+    self.tranformFadeViewOne.animationGapDuration = 0.1f;
+    
+    [self.view addSubview:self.tranformFadeViewOne];
+
+    
+    // 图片2
+    self.tranformFadeViewTwo                 = [[TranformFadeView alloc] initWithFrame:self.view.bounds];
+    self.tranformFadeViewTwo.contentMode     = UIViewContentModeScaleAspectFill;
+    self.tranformFadeViewTwo.image           = [UIImage imageNamed:@"2"];
+    self.tranformFadeViewTwo.verticalCount   = 5;
+    self.tranformFadeViewTwo.horizontalCount = 5;
+    self.tranformFadeViewTwo.center          = self.view.center;
+    [self.tranformFadeViewTwo buildMaskView];
+    
+    self.tranformFadeViewTwo.fadeDuradtion        = 1.f;
+    self.tranformFadeViewTwo.animationGapDuration = 0.1f;
+    
+    [self.view addSubview:self.tranformFadeViewTwo];
+    [self.tranformFadeViewTwo fadeAnimated:YES];
+    
+    
+    
+    // 定时器
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:6
+                                                  target:self
+                                                selector:@selector(timerEvent)
+                                                userInfo:nil
+                                                 repeats:YES];
+    self.type  = TYPE_ONE;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)timerEvent {
+    if (self.type == TYPE_ONE) {
+        self.type = TYPE_TWO;
+        
+        [self.view sendSubviewToBack:self.tranformFadeViewTwo];
+        [self.tranformFadeViewTwo showAnimated:NO];
+        [self.tranformFadeViewOne fadeAnimated:YES];
+        
+    } else {
+        self.type = TYPE_ONE;
+        
+        [self.view sendSubviewToBack:self.tranformFadeViewOne];
+        [self.tranformFadeViewOne showAnimated:NO];
+        [self.tranformFadeViewTwo fadeAnimated:YES];
+
+    }
 }
 
 @end
